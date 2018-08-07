d6c522b1b598d68c4059d0e6491db84692d7492a
Noppen - Kleist, Katharina van <katharina.vannoppen-kleist@kpn.com>
2018-08-07 11:17:56 +0200

    update simpler

--- content_filter_anja.py
+++ content_filter_anja.py
@@ -1,12 +1,12 @@
 import random
 import tkinter as tk
-
+import itertools
 import numpy as np
 import pandas as pd
 
 DATAFOLDER = "C:/Users/kleis500/Eclipse workspace/RecSys/data/cog_coach/"
 
-# Read in the data
+#read data
 df = pd.read_excel(DATAFOLDER + "acties_features_rated.xlsx")
 
 X = []
@@ -24,7 +24,6 @@ for row in df["Anja"]:
     else:
         rij.append(0)
 
-# df"r_ij"] = df["Anja"].apply(lambda x: np.isnan(x) and 0 or 1)
 df["Rating"] = rij
 
 # Make new dataframe with rated items
@@ -37,16 +36,6 @@ for i in anja_matrix["Item Profile"]:
         summed_profile[count] = summed_profile[count] + j
         count += 1
 
-# calculate star rating (weight for item profiles)
-# count number of feature occurence for 1 user
-am_binnen = anja_matrix['binnen'].loc[anja_matrix['binnen'] > 0].count()
-am_buiten = anja_matrix['buiten'].loc[anja_matrix['buiten'] > 0].count()
-am_alleen = anja_matrix['alleen'].loc[anja_matrix['alleen'] > 0].count()
-am_samen = anja_matrix['samen'].loc[anja_matrix['samen'] > 0].count()
-am_kort = anja_matrix['kort'].loc[anja_matrix['kort'] > 0].count()
-am_lang = anja_matrix['lang'].loc[anja_matrix['lang'] > 0].count()
-count_anja = [am_binnen, am_buiten, am_alleen, am_samen, am_kort, am_lang]
-
 # Extract raw ratings for user per feature
 am_binnen_rating = list(anja_matrix['Anja'].loc[anja_matrix['binnen'] > 0])
 am_buiten_rating = list(anja_matrix['Anja'].loc[anja_matrix['buiten'] > 0])
@@ -58,12 +47,15 @@ am_lang_rating = list(anja_matrix['Anja'].loc[anja_matrix['lang'] > 0])
 raw_rating_list = [am_binnen_rating, am_buiten_rating, am_alleen_rating, am_samen_rating, am_kort_rating,
                    am_lang_rating]
 
+flat_ratings = list(itertools.chain.from_iterable(raw_rating_list))
+mean_ratings = sum(flat_ratings)/len(flat_ratings)
+
 # Normalizae raw ratings for user
 normalized_ratings = []
 for i in raw_rating_list:
     temp_list = []
     for j in i:
-        j -= 3
+        j -= mean_ratings
         temp_list.append(j)
     normalized_ratings.append(temp_list)
 
@@ -72,10 +64,9 @@ sum_ratings = []
 for i in normalized_ratings:
     sum_ratings.append(sum(i))
 
-weights_anja = np.array(sum_ratings) / np.array(count_anja)
-summed_weighted_profiles = weights_anja * summed_profile
-# Personalized user profiles
-weighted_anja_user_profile = summed_weighted_profiles / np.array([len(df), len(df), len(df), len(df), len(df), len(df)])
+# # Personalized user profiles
+weighted_anja_user_profile = (np.array(sum_ratings) * np.array(summed_profile))/np.array([len(df), len(df), len(df), len(df), len(df), len(df)])
+
 
 similarities = []
 # Compute the cosine similarity between item profile and user profile
@@ -83,7 +74,7 @@ for item in df['Item Profile']:
     cosine_sim = (weighted_anja_user_profile * item) / (
             np.linalg.norm(weighted_anja_user_profile) * np.linalg.norm(item))
 
-    #     cosine_sim = linear_kernel(weighted_anja_user_profile, item)
+    #cosine_sim = linear_kernel(weighted_anja_user_profile, item)
     similarities.append(cosine_sim)
 
 summed_cos = []
@@ -92,9 +83,9 @@ for item in similarities:
 
 df['summed_cos'] = summed_cos
 
-predictions = df.sort_values(by='summed_cos', ascending=False)
+df.sort_values(by='summed_cos', ascending=False, inplace = True)
 
-top5 = list(predictions["Actie"][:5])
+top5 = list(df["Actie"][:5])
 advice = 'Hi Anja, ' + random.choice(top5)
 
 window = tk.Tk()
