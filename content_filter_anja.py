922b45830a8f0326036887062d7e47731b213e3f
Hoef, Michel van de <michel.vandehoef@kpn.com>
2018-08-08 14:27:29 +0200

    test

--- content_filter_anja.py
+++ content_filter_anja.py
@@ -4,6 +4,7 @@ import itertools
 import numpy as np
 import pandas as pd
 ## dit is een check
+# en nu nog verder
 DATAFOLDER = "C:/Users/kleis500/Eclipse workspace/RecSys/data/cog_coach/"
 
 #read in data
